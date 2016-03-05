7c88a19aa07b2cae399242b70ba1f2f79fe68af9
Holly French <hollyafrench90@gmail.com>
2016-03-05 12:00:03 -0800

    Add database ingestion and models

--- parks_discovery/parks_discovery/settings.py
+++ parks_discovery/parks_discovery/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'parks_discovery',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
